d52c6bf9fc15fa0a927adccfa8d0392e5516478f
Egor Dmitriev <egordmitriev2@gmail.com>
2018-07-11 20:57:54 +0200

    Modified tatum feature extraction. Only tatum len in ms

--- src/features/tatums.py
+++ src/features/tatums.py
@@ -2,16 +2,13 @@ import librosa, librosa.display
 import matplotlib.pyplot as plt
 
 
-def extract_tatums(x, sr, tatum_size=16, plot=False):
+def extract_tatum_size(x, sr, tatum_size=16):
     t = librosa.beat.tempo(x, sr, start_bpm=70.)
     t *= tatum_size
-    tatum_length_ms = 60000/t
-    samples_per_tatum = tatum_length_ms * sr
-    print(tatum_length_ms)
-    print(samples_per_tatum)
+    tatum_length_ms = 60000/t[0]
     return tatum_length_ms
 
 
-x, sr = librosa.load('../../data/raw/dream_on.wav')
-t = extract_tatums(x, sr, plot=True)
-plt.show()
+# x, sr = librosa.load('../../data/raw/dream_on.wav')
+# t = extract_tatums(x, sr, plot=True)
+# plt.show()
